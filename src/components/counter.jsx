a59d66297a09e8c83a0f6f4a206314ab535a8dd1
Julius Hsu <juliushsu07@gmail.com>
2022-11-17 15:50:31 -0500

    CreatCounter component with a function that formats the count number.

---
+++ src/components/counter.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        address: {
+            street: ''
+        }
+    };
+
+    render() { 
+        return ( 
+            <React.Fragment>
+                <span>{this.formatCount()}</span>
+                <button>Increment</button>
+            </React.Fragment>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? <h1>Zero</h1> : count;
+    }
+}
+ 
+export default Counter;
