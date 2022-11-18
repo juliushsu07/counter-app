8610547bfc927a5b5be34f41af3e1c6a49411ce7
Julius Hsu <juliushsu07@gmail.com>
2022-11-17 20:48:50 -0500

    Add props from Counters component to Counter, in order to initialize counter values.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         tags: []
     };
 
@@ -11,14 +11,19 @@ class Counter extends Component {
         fontWeight: "bold"
     };
 
-    handleIncrement = () => {
-        this.setState({ count: this.state.count + 1})
+    getBadgeClasses() {
+        let classes = "badge m-2 bg-";
+        classes += (this.state.value === 0) ? "warning" : "primary";
+        return classes;
     }
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-        
-        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    formatCount() {
+        const { value } = this.state;
+        return value === 0 ? <h1>Zero</h1> : value;
+    }
+
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1})
     }
 
     render() { 
@@ -28,18 +33,7 @@ class Counter extends Component {
                 <button onClick={ () => this.handleIncrement()} className="btn btn btn-secondary">Increment</button>
             </div>
         );
-    }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 bg-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? <h1>Zero</h1> : count;
-    }
+    }  
 }
  
 export default Counter;
